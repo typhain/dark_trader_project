bdd1954d3f25e52ab0e4c6afb013da196e4820b3
typhain <typhaine.charrat@gmail.com>
2019-07-18 14:11:27 +0200

    initialisation dossier

---
+++ lib/dark_trader.rb
@@ -0,0 +1,41 @@
+require 'open-uri'
+	require 'nokogiri'
+
+def get_symbols
+      symbols = []
+    doc = Nokogiri::HTML(open("https://coinmarketcap.com/all/views/all/"))
+  	doc.xpath('//td[3]').each do |node|
+  	  symbols << node.text
+  	end
+    symbols
+end
+
+def get_prices
+      prices = []
+    doc = Nokogiri::HTML(open("https://coinmarketcap.com/all/views/all/"))
+  	doc.xpath('//td[5]/a').each do |node|
+  	  prices << node.text
+  	end
+    prices
+end
+
+def combine
+
+  myhash = Hash[get_symbols.zip get_prices]
+
+  Hash.class_eval do
+    def split_into(divisions)
+      count = 0
+      inject([]) do |final, key_value|
+        final[count%divisions] ||= {}
+        final[count%divisions].merge!({key_value[0] => key_value[1]})
+        count += 1
+        final
+      end
+    end
+  end
+    a = [myhash.split_into(get_symbols.size)]
+    puts a
+end
+
+combine
